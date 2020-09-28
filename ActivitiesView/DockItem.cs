4a5a037be8209a277c4dca0123140fb0885cc690
Kengo Ide <kasumikagari@gmail.com>
2020-09-28 20:35:40 +0900

    Make DockItem.Image a dependency property.

--- ActivitiesView/DockItem.cs
+++ ActivitiesView/DockItem.cs
@@ -10,23 +10,27 @@ using System.Windows.Media.Imaging;
 
 namespace ActivitiesView
 {
-    class DockItem
+    class DockItem : DependencyObject
     {
         private string _filePath;
-        private BitmapSource _bitmapSource;
-        
-        public DockItem(string executableFilePath)
+
+        private static readonly DependencyPropertyKey ImagePropertyKey =
+            DependencyProperty.RegisterReadOnly("Image", typeof(BitmapSource), typeof(DockItem), new PropertyMetadata());
+        public static readonly DependencyProperty ImageProperty = ImagePropertyKey.DependencyProperty;
+
+        public DockItem(string executableFilePath) : base()
         {
             _filePath = executableFilePath;
             Win32.IShellItemImageFactory imageFactory =
                 (Win32.IShellItemImageFactory)Win32.SHCreateItemFromParsingName(
                     _filePath, IntPtr.Zero, typeof(Win32.IShellItemImageFactory).GUID);
             IntPtr hBitmap = imageFactory.GetImage(new Win32.SIZE(256, 256), Win32.SIIGBF_BIGGERSIZEOK);
-            _bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, new Int32Rect(), BitmapSizeOptions.FromEmptyOptions());
+            BitmapSource bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, new Int32Rect(), BitmapSizeOptions.FromEmptyOptions());
+            SetValue(ImagePropertyKey, bitmapSource);
         }
 
         public string FilePath { get => _filePath; }
-        public BitmapSource Image { get => _bitmapSource; }
+        public BitmapSource Image { get => (BitmapSource)GetValue(ImageProperty); }
 
         public void Launch()
         {
